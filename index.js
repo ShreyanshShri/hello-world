26214f17011ccba1cb19b61de38649743471ec84
ShreyanshShri <62863886+ShreyanshShri@users.noreply.github.com>
2021-01-06 01:33:22 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+function con() {
+   this.func = (stuff) => {
+       console.log(stuff)
+       return stuff
+   }
+    this.log = this.func
+    this.लोग = this.func
+    this.журнал = this.func
+    this.ログ = this.func
+}
+
+const कौंन्सोल = new con()
+const consola = new con()
+const приставка = new con()
+const コンソール = new con()
+
+// I used google translate so I'm not sure if these are correct translation or not.
+// So if you find any mistake please let me know by breating a PR or an issue
+
+console.log("Hello World")
+कौंन्सोल.लोग("नमस्ते दुनिया")
+consola.log("Hola Mundo")
+приставка.журнал("Привет, мир")
+コンソール.ログ("こんにちは世界") you
